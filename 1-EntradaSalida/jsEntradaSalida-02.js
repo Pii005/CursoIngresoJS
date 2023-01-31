733cb5176dfddea1c6f41d08728f31694851024e
pii005 <pilar.flores04@gmail.com>
2023-01-31 11:36:46 -0300

    ejercicio 2

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,10 @@
-/*
+/* Pilar Flores 
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	let nombrePersona; //declaracion de variable
+	nombrePersona = prompt("¿Cuál es tu nombre?");
+	alert(nombrePersona);
 }
 
