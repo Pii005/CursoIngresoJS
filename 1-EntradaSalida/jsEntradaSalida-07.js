9a0d95f5c8240ac8ab1c680da268655ac39676e5
pii005 <pilar.flores04@gmail.com>
2023-02-01 11:49:26 -0300

    hasta 7

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -1,10 +1,11 @@
-/*
+/*Pilar Flores 
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
+	
 	alert("ok");	
 }
 
--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -1,4 +1,4 @@
-/*
+/*Pilar Flores 
 Debemos lograr tomar el importe por ID ,
 transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
